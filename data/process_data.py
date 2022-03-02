90729152fa0d23981c8441a7552da30284e857c6
Ahmed <65422857+zachdotai@users.noreply.github.com>
2022-03-02 14:15:58 +0300

    Fixed error when saving processed data to existing database

--- data/process_data.py
+++ data/process_data.py
@@ -73,7 +73,7 @@ def save_data(df, database_filename):
     '''
     
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('labelled_messages', engine, index=False)
+    df.to_sql('labelled_messages', engine, index=False, if_exists='replace')
 
 
 def main():
